2b4cb3ba848697dbdfe38c267560241cf23e58aa
Gregoire Gherardi <gherardigregoire@gmail.com>
2024-11-25 15:12:11 +0100

    adds

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
 @app.route("/prompt", methods = ['POST'])
 def prompt():
     if request.method == 'POST':
