376f673a9105fcbc367fba3b615e0da68f045d27
ArmanddeCacqueray <armanddecacqueray@sfr.fr>
2025-01-23 15:40:50 +0100

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -75,31 +75,7 @@ def home():
 
 
 ###############################################
-def compterendu_basededonnnee():
-    #with open(filename, 'r', encoding='utf-8', errors='ignore') as f:
-    #    transcription = f.read()
-        # Appel à l'API OpenAI pour générer le résumé
-    message_history = []
-    filename = os.path.join(os.path.dirname(__file__), "modele_basededonnee.txt")
-    document = read_text(filename)
-    chunks = split_text(document)
-    filename2 = os.path.join(os.path.dirname(__file__), "transcription.txt")
-    document2 = read_txt(filename2)
-    chunks2 = split_text(document2)
-    message_history.append({"role": "user", "content": chunks[0]})
-    message_history.append({"role": "user", "content": chunks2[0]})
-    message_history.append({"role": "user",
-                                "content": "génère un texte permettant de produire un compte rendu sous forme de base de donnée  a partir de la trancription de reunion, en respectant le format imposé"})
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo", messages=message_history, max_tokens=450
-    )
 
-    summary = response.choices[0].message['content']
-    text_file_path = filename2.replace('.txt', '_resume.txt')
-    with open(text_file_path, 'w') as text_file:
-        text_file.write(summary)
-    print(f"resume sauvegardée dans {text_file_path}")
-    return summary
 ##############################################
     
 if __name__ == '__main__':
